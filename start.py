4678b84cc41a14c9800b3831da89f754af663954
darklhg-creator <darklhg@naver.com>
2026-03-07 15:38:58 +0900

    Update start.py

--- start.py
+++ start.py
@@ -5,26 +5,14 @@ url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/get
 
 params = {
     "serviceKey": API_KEY,
-    "numOfRows": "30",
+    "numOfRows": "5",
     "pageNo": "1",
     "resultType": "json",
-    "likeSrtnCd": "156100"  # 엘앤케이바이오
+    "likeSrtnCd": "156100",
+    "basDt": "20260306"  # 3월 6일 데이터 있는지 확인
 }
 res = requests.get(url, params=params, timeout=10)
 data = res.json()
-items = data['response']['body']['items']['item']
-if isinstance(items, dict):
-    items = [items]
-
-# 날짜순 정렬
-items = sorted(items, key=lambda x: x.get('basDt', ''))
-
-for item in items:
-    print(f"날짜: {item.get('basDt')}, 종가: {item.get('clpr')}")
-
-# MA20 계산
-prices = [float(str(item.get('clpr','0')).replace(',','')) for item in items[-20:]]
-ma20 = sum(prices) / 20
-current = prices[-1]
-disparity = round((current / ma20) * 100, 2)
-print(f"\n현재가: {current}, MA20: {round(ma20,2)}, 이격도: {disparity}%")
+print("총 건수:", data['response']['body']['totalCount'])
+items = data['response']['body']['items']
+print("데이터:", items)
