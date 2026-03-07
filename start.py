5d51004664eed7f6b458fd983c45d0896167448e
darklhg-creator <darklhg@naver.com>
2026-03-07 15:40:52 +0900

    Update start.py

--- start.py
+++ start.py
@@ -3,16 +3,20 @@ import requests
 API_KEY = "62e0d95b35661ef8e1f9a665ef46cc7cd64a3ace4d179612dda40c847f6bdb7e"
 url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo"
 
+# 날짜 없이 최신 데이터 조회
 params = {
     "serviceKey": API_KEY,
-    "numOfRows": "5",
+    "numOfRows": "3",
     "pageNo": "1",
     "resultType": "json",
-    "likeSrtnCd": "156100",
-    "basDt": "20260306"  # 3월 6일 데이터 있는지 확인
+    "likeSrtnCd": "156100"
 }
 res = requests.get(url, params=params, timeout=10)
 data = res.json()
-print("총 건수:", data['response']['body']['totalCount'])
-items = data['response']['body']['items']
-print("데이터:", items)
+items = data['response']['body']['items']['item']
+if isinstance(items, dict):
+    items = [items]
+
+items = sorted(items, key=lambda x: x.get('basDt', ''), reverse=True)
+for item in items:
+    print(f"날짜: {item.get('basDt')}, 종가: {item.get('clpr')}")
