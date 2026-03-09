35316bb6af5c9faca7b2d77ec3f27a7084bb2c97
darklhg-creator <darklhg@naver.com>
2026-03-09 20:18:05 +0900

    Update start.py

--- start.py
+++ start.py
@@ -1,16 +1,5 @@
-import requests
+import FinanceDataReader as fdr
 
-KRX_API_KEY = "5AB85D9D43EA4FAA9BC1907303BAFDC2C0377C5B"
-
-# 올바른 KRX OpenAPI URL로 테스트
-url = "https://openapi.krx.co.kr/contents/COM/GenerateOTP.jspx"
-params = {
-    "AUTH_KEY": KRX_API_KEY,
-    "bld": "dbms/MDC/STAT/standard/MDCSTAT01501",
-    "name": "fileDown",
-    "filetype": "json"
-}
-
-res = requests.get(url, params=params, timeout=10)
-print("상태코드:", res.status_code)
-print("응답:", res.text[:500])
+df = fdr.DataReader('156100', '2026-02-01')
+print(df.tail(5))
+print("최신날짜:", df.index[-1])
